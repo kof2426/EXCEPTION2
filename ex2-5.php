94cdc1735ded9d0500df6f1e9b028be7b60edd58
kof2426 <kof2426@naver.com>
2020-12-31 23:10:47 +0900

    Add PHP File

---
+++ ex2-5.php
@@ -0,0 +1,13 @@
+<?php
+	echo "<br> 1. 정수형 변수 선언입니다...<br>";
+		$var_int = 150;
+		echo "\$var_int = $var_int <br><br>";
+		
+	echo "<br> 2. 실수형 변수 선언입니다...<br>";
+		$var_float = 3.141592;
+		echo "\$var_float = $var_float <Br><br>";
+		
+	echo "<br> 3. 문자열형 변수 선언입니다...<br>";
+		$var_char = "웹언어";
+		echo "\$var_float = $var_char <br><br>";
+?>
